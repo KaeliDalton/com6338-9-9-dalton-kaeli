e40a990165a2ee4c3a27c04a396183266a605074
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-04-02 12:55:49 -0400

    arrow function and template literals refactor

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Your code here
+
 /*Needs
 Convert a 'var' to a 'const' or 'let'(converted all)
+
 convert a promise based ('.then') to 'asynch' or 'await'
-convert a 'function' to an 'arrow function'
-convert string concatenation into template literals and string interpolation
+
+convert a 'function' to an 'arrow function' (set final function of displayData to an arrow function)
+
+convert string concatenation into template literals and string interpolation (template literals done no idea what a string interpolation is)
+
 convert some 'object-related code' into 'ES6 destructuring'
 */
 // Global Constants
@@ -17,7 +21,7 @@ form.onsubmit = function (e) { //turn asynch??
     e.preventDefault()
     const URL = "https://api.openweathermap.org/data/2.5/weather?q="
     let city = this.search.value.trim()
-    const usedURL = URL + city + "&units=imperial&appid=" + API
+    const usedURL = `${URL}${city}&units=imperial&appid=${API}`
     //if no input given, clear form
     if ((!city) || (search.value = "")){
         city = ''
@@ -39,8 +43,8 @@ form.onsubmit = function (e) { //turn asynch??
         })
 }
 
-//display info function
-function displayData(data) {
+//turn a function (displayData) into an arrow function
+const displayData = (data) => {
     city = ""
     weather.innerHTML = ""
     search.value = ''
@@ -53,7 +57,7 @@ location.textContent = data.name + " , " + data.sys.country
 const mapLink = document.createElement('a')
 const lat = data.coord.lat
 const lon = data.coord.lon
-const googleMap = "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lon
+const googleMap = `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`
 weather.appendChild(mapLink)
 mapLink.textContent = "Click to View Map"
 mapLink.href = googleMap
@@ -62,7 +66,7 @@ mapLink.target = "_BLANK"
 //show weather condition icon
 const icon = document.createElement('img')
 const iconCode = data.weather[0].icon
-const iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
 icon.src = iconURL
 icon.alt = data.name
 weather.appendChild(icon)
@@ -75,13 +79,13 @@ weather.appendChild(condition)
 //show current temperature
 const temperature = document.createElement('p')
 const temperatureNumber = data.main.temp
-temperature.textContent = "Current:  " + temperatureNumber + '° F'
+temperature.textContent = `Current: ${temperatureNumber}° F`
 weather.appendChild(temperature)
 
 //show feels like temperature
 const feelsLike = document.createElement('p')
 const feelsLikeTemp = data.main.feels_like
-feelsLike.textContent = "Feels like:  " + feelsLikeTemp + '° F'
+feelsLike.textContent = `Feels like: ${feelsLikeTemp}° F`
 weather.appendChild(feelsLike)
 
 //show time updated
@@ -91,6 +95,6 @@ const time = date.toLocaleTimeString('en-US', {
     hour: 'numeric', 
     minute: '2-digit'
 })
-dateTime.textContent = 'Last Updated:  ' + time
+dateTime.textContent = `Last Updated: ${time}`
 weather.appendChild(dateTime)
 }
